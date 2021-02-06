3b31286056b3feea02c142a403c4558c4f2cc727
Gustavo Morilla <gu_morilla@hotmail.com>
2021-02-06 01:22:42 -0300

    installing main packages

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './layouts/Header';
+
+
+function MyApp({ Component, pageProps }) {
+  return (
+  <>
+  <Header />
+  <Component { ...pageProps} />
+  </>
+  )
+}
+
+export default MyApp
