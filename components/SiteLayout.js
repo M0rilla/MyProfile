86baec1cc7e66010123d386d87db78493fe47e31
Gustavo Morilla <gu_morilla@hotmail.com>
2021-02-24 02:18:08 -0300

    learning about viewport and media queries for responsive design

--- components/SiteLayout.js
+++ components/SiteLayout.js
@@ -1,10 +1,15 @@
 import NavItem from "../components/NavItem.js";
 import Nav from "./Nav.js";
 import Link from 'next/link';
+import Head from 'next/head'
 
 const SiteLayout = ({ children }) => (
   <div className="mt-8 mx-auto">
     <div>
+    <Head>
+        <title>Gustavo Morilla</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <div>
         <h1 className="text-center">          
           <Link href="/">
